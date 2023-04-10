f737160dd06a813faa33ced269da0a23e10872ea
shineHale <3116227229@qq.com>
2023-04-10 19:51:28 +0800

    finish Part V all the codes

--- Effective C++/codes/Part V/src/main.cpp
+++ Effective C++/codes/Part V/src/main.cpp
@@ -3,8 +3,6 @@
 using namespace std;
 
 int main(){
-    auto pad = new Address("HUST B503");
-    cout << pad->address() << endl;
     auto pp = new RealPerson("Shinehale", Date(Month::July(), Day(30), Year(2003)), Address("HUST C103"));
     cout << "Name:" << pp->name() << endl;
     cout << "BirthDate: " << pp->brithDate() << endl;
